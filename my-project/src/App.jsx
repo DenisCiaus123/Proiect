75d97071bd2fe83bd22cc2addff3d1113bda6217
aevrabies <andreea-elena.vrabies@ubisoft.com>
2025-06-16 08:52:45 +0000

    added css, new elements

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import logo from "./images/logo_light.png";
 import { Body } from "./components/Body";
+import './global.css';
 
 export const App = () => {
   return (
--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -1,11 +1,15 @@
+import styles from './headerStyle.module.css';
+
 export const Header = ({ title, subtitle, logo }) => {
     return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <img src={logo} alt="Logo" className="logo-img" />
+        <nav className={styles.navbar}>
+            <div className={styles.navbar_logo}>
+                <img src={logo} alt="Logo" className={styles.logo_img}/>
             </div>
-            <h1 className="navbar-title">{title}</h1>
-            <h2 className="navbar-subtitle">{subtitle}</h2>
+            <div className={styles.title_container}>
+                <h1 className={styles.navbar_title}>{title}</h1>
+                <h2 className={styles.navbar_subtitle}>{subtitle}</h2>
+            </div>
         </nav>
     );
 };
