b029c2f3a45ead3d439131b508ab489b261c37a8
Denis-Bogdan Ciaus <denis-bogdan.ciaus@ubisoft.com>
2025-06-16 17:40:56 +0300

    Created methods to display questions and answers for the first retrieved entry. updated the logic for displaying elements depending on entry type.

--- my-project/src/components/Body.jsx
+++ my-project/src/components/Body.jsx
@@ -1,29 +1,120 @@
-import styles from './bodyStyle.module.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./bodyStyle.module.css";
 
 export const Body = () => {
+    const [loading, setLoading] = useState(false);
+    const [fetchedEntries, setFetchedEntries] = useState([]);
+    const [entry, setEntry] = useState(null);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [score, setScore] = useState(0);
+    const [answers, setAnswers] = useState([]);
+
+    function decodeHtml(htmlCode) {
+        const txt = document.createElement("textarea");
+        txt.innerHTML = htmlCode;
+        return txt.value;
+    };
+
+    const addPoints = (type, difficulty) => {
+        let points = 0;
+
+        if (type === "multiple") {
+            points += 2;
+        }
+        else {
+            points += 0;
+        }
+
+        if (difficulty === "easy") {
+            points += 1;
+        }
+        else if (difficulty === "medium") {
+            points += 2;
+        }
+        else {
+            points += 3;
+        }
+
+        return points;
+    }
+
+    const checkAnswer = (e, entry) => {
+        const answerText = e.target.textContent;
+        if (entry.correct_answer === answerText) {
+            setScore(prevScore => prevScore + addPoints(entry.type, entry.difficulty));
+        }
+        else setScore(1000000);
+    };
+
+    const fetchData = async () => {
+        if (loading) return;
+        setLoading(true);
+        try {
+            const response = await axios.get("https://opentdb.com/api.php?amount=50");
+            setFetchedEntries(response.data.results);
+            console.log("Fetched trivia data:", response.data.results);
+            setEntry(response.data.results[0]);
+        }
+        catch (error) {
+            console.error("Error fetching data:", error);
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+
+    function shuffle(array) {
+        let currentIndex = array.length;
+
+        while (currentIndex != 0) {
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+        return array;
+    }
+
+    useEffect(() => {
+        if (entry) {
+            setAnswers(shuffle([...entry.incorrect_answers, entry.correct_answer]));
+            console.log("Answers:", answers);
+        }
+    }, [entry]);
+
     return (
         <div className={styles.main_content}>
             <div>
-                <div className={styles.score_container}>
-                    <span> score: </span>
-                    <span> 125_ph</span>
-                </div>
-                <div>
-                    <h3 className={styles.question}>question</h3>
-                </div>
-                <div className={styles.container_answers}>
-                    {/* button grid */}
-                    <button className={styles.button_answer}>Button PH</button>
-                    <button className={styles.button_answer}>Button PH</button>
-                    <button className={styles.button_answer}>Button PH</button>
-                    <button className={styles.button_answer}>Button PH</button>
-                    <p> Correct/wrong</p>
-                {/* 2 new classes placeholder rasp corect-gresit div*/}
-                </div>
-                <input type="text" placeholder="text PH" className={styles.body_input}></input>
-                <div>
-                    <button className={styles.button_search}>Search</button>
-                </div>
+                <h3 className={styles.question}>{entry ? decodeHtml(entry.question) : (<><button className={styles["button-start"]} onClick={fetchData}>Start Quizz</button></>)}</h3>
+            </div>
+            {entry ? (<>
+                {entry.type === "multiple" ?
+                    (<>
+                        <div className={styles.container_multi_answer}>
+                            <button className={styles["button-answer"]} onClick={(e) => checkAnswer(e, entry)}>{answers[0]}</button>
+                            <button className={styles["button-answer"]} onClick={(e) => checkAnswer(e, entry)}>{answers[1]}</button>
+                            <button className={styles["button-answer"]} onClick={(e) => checkAnswer(e, entry)}>{answers[2]}</button>
+                            <button className={styles["button-answer"]} onClick={(e) => checkAnswer(e, entry)}>{answers[3]}</button>
+                        </div>
+                    </>) :
+                    (<>
+                        <div className={styles.container_bool_answer}>
+                            <button className={styles["button-answer"]} onClick={(e) => checkAnswer(e, entry)}>{answers[0]}</button>
+                            <button className={styles["button-answer"]} onClick={(e) => checkAnswer(e, entry)}>{answers[1]}</button>
+                        </div>
+                    </>)
+                }</>) :
+                (<></>)}
+
+            <div className={styles.score}>
+                <span>Score:</span>
+                <span>{score}</span>
+            </div>
+
+            <input type="text" placeholder="text PH" className={styles["body-input"]} />
+            <div>
+                <button className={styles["button-search"]}>Search</button>
             </div>
         </div>
     );
