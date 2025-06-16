857fb5b3cbcfa5c0c95bc4b43feb3368196ef15c
aevrabies <andreea-elena.vrabies@ubisoft.com>
2025-06-16 15:03:36 +0000

    updated styles for header footer body, updated header and footer, proper classes and updated links

--- my-project/src/components/Footer.jsx
+++ my-project/src/components/Footer.jsx
@@ -4,12 +4,9 @@ export const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <footer>
-            <ul className={styles.footer_list}>
-                {/* remove bullets
-                custom colors links
-                copyright size*/}
-                <li className={styles.footer_link}><a href="">Link 1 PH</a></li>
-                <li className={styles.footer_link}><a href="">Link 2 PH</a></li>
+            <ul className={styles["footer-list"]}>
+                <li><a className={styles["footer-link"]} href="https://opentdb.com/api_config.php" target="_blank" rel="noopener noreferrer">Open Trivia</a></li>
+                <li><a className={styles["footer-link"]} href="https://coolors.co" target="_blank" rel="noopener noreferrer">Coolors</a></li>
                 <li className={styles.copyright}>Copyright {currentYear}</li>
             </ul>
         </footer>
--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -3,12 +3,12 @@ import styles from './headerStyle.module.css';
 export const Header = ({ title, subtitle, logo }) => {
     return (
         <nav className={styles.navbar}>
-            <div className={styles.navbar_logo}>
-                <img src={logo} alt="Logo" className={styles.logo_img}/>
+            <div className={styles["navbar-logo"]}>
+                <img src={logo} alt="Logo" className={styles["logo-img"]}/>
             </div>
-            <div className={styles.title_container}>
-                <h1 className={styles.navbar_title}>{title}</h1>
-                <h2 className={styles.navbar_subtitle}>{subtitle}</h2>
+            <div className={styles["title-container"]}>
+                <h1 className={styles["navbar-title"]}>{title}</h1>
+                <h2 className={styles["navbar-subtitle"]}>{subtitle}</h2>
             </div>
         </nav>
     );
