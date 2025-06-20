51571f1f26ace2ab55922ee43ee612e935c85ae1
Denis-Bogdan Ciaus <denis-bogdan.ciaus@ubisoft.com>
2025-06-20 11:17:58 +0300

    Switched questions amount to input number instead of drop down selection. updated visuals for some elements

--- my-project/src/components/QuizMain.jsx
+++ my-project/src/components/QuizMain.jsx
@@ -6,104 +6,104 @@ import { categorySymbols } from "./CategorySymbols";
 import { decodeHtml, addPoints, shuffle, applyDifficultyStyle } from "./utils";
 
 export const QuizMain = ({
-  entity,
-  setEntityIndex,
-  score,
-  setScore,
-  entityIndex,
-  totalQuestions,
-  restartQuizSameSettings,
+    entity,
+    setEntityIndex,
+    score,
+    setScore,
+    entityIndex,
+    totalQuestions,
+    restartQuizSameSettings,
 }) => {
-  const [answers, setAnswers] = useState([]);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [showResult, setShowResult] = useState(false);
+    const [answers, setAnswers] = useState([]);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [showResult, setShowResult] = useState(false);
 
-  useEffect(() => {
-    if (entity) {
-      const allAnswers = shuffle([
-        ...entity.incorrect_answers,
-        entity.correct_answer,
-      ]);
-      setAnswers(allAnswers.map(decodeHtml));
-      setSelectedAnswer(null);
-      setShowResult(false);
-    }
-  }, [entity]);
+    useEffect(() => {
+        if (entity) {
+            const allAnswers = shuffle([
+                ...entity.incorrect_answers,
+                entity.correct_answer,
+            ]);
+            setAnswers(allAnswers.map(decodeHtml));
+            setSelectedAnswer(null);
+            setShowResult(false);
+        }
+    }, [entity]);
 
-  const checkAnswer = (e) => {
-    if (showResult) return;
-    const answerText = e.target.textContent;
-    setSelectedAnswer(answerText);
-    setShowResult(true);
-    if (entity.correct_answer === answerText) {
-      setScore((prev) => prev + addPoints(entity.type, entity.difficulty));
-    }
-    setTimeout(() => {
-      setEntityIndex((prev) => prev + 1);
-    }, 3000);
-  };
-  const getButtonClass = (answer) => {
-    if (!showResult) return styles.answerButton;
-    if (answer === decodeHtml(entity.correct_answer))
-      return `${styles.answerButton} ${styles.correct}`;
-    if (answer === selectedAnswer)
-      return `${styles.answerButton} ${styles.incorrect}`;
-    return `${styles.answerButton} ${styles.restButtons}`;
-  };
-  const symbolSrc =
-    categorySymbols[decodeHtml(entity.category)] ||
-    "src/images/categories/symbol_any.png";
+    const checkAnswer = (e) => {
+        if (showResult) return;
+        const answerText = e.target.textContent;
+        setSelectedAnswer(answerText);
+        setShowResult(true);
+        if (entity.correct_answer === answerText) {
+            setScore((prev) => prev + addPoints(entity.type, entity.difficulty));
+        }
+        setTimeout(() => {
+            setEntityIndex((prev) => prev + 1);
+        }, 1500);
+    };
+    const getButtonClass = (answer) => {
+        if (!showResult) return styles.answerButton;
+        if (answer === decodeHtml(entity.correct_answer))
+            return `${styles.answerButton} ${styles.correct}`;
+        if (answer === selectedAnswer)
+            return `${styles.answerButton} ${styles.incorrect}`;
+        return `${styles.answerButton} ${styles.restButtons}`;
+    };
+    const symbolSrc =
+        categorySymbols[decodeHtml(entity.category)] ||
+        "src/images/categories/symbol_any.png";
 
-  return (
-    <>
-      <div
-        className={`${styles.card} ${
-          styles[applyDifficultyStyle(entity.difficulty)]
-        }`}
-      >
-        <div className={styles.questionContainer}>
-          <img
-            src={symbolSrc}
-            alt="Category Symbol"
-            className={styles.categorySymbol}
-          />
-          <div className={styles.bar} />
-          <h3 className={shared.quizQuestion}>{decodeHtml(entity.question)}</h3>
-        </div>
-
-        <div
-          className={
-            entity.type === "multiple"
-              ? styles.multipleChoiceGrid
-              : styles.booleanChoiceGrid
-          }
-        >
-          {answers.map((answer, index) => (
-            <button
-              key={index}
-              className={getButtonClass(answer)}
-              onClick={checkAnswer}
-              disabled={showResult}
+    return (
+        <>
+            <div
+                className={`${styles.card} ${styles[applyDifficultyStyle(entity.difficulty)]
+                    }`}
             >
-              {answer}
-            </button>
-          ))}
-        </div>
-        <span>
-          Question Number: {entityIndex + 1} of {totalQuestions}
-        </span>
-        <div className={styles.scorePanel}>
-          <span>Score: {score}</span>
-          <div>
-            <button
-              className={styles.resetQuizButton}
-              onClick={restartQuizSameSettings}
-            >
-              Restart Quiz
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                <span className={styles.questionNumber}>
+                    {`Remaining questions: ${totalQuestions - entityIndex}`}
+                </span>
+                <div className={styles.questionContainer}>
+                    <img
+                        src={symbolSrc}
+                        alt="Category Symbol"
+                        className={styles.categorySymbol}
+                    />
+                    <div className={styles.bar} />
+                    <h3 className={shared.quizQuestion}>{decodeHtml(entity.question)}</h3>
+                </div>
+
+                <div
+                    className={
+                        entity.type === "multiple"
+                            ? styles.multipleChoiceGrid
+                            : styles.booleanChoiceGrid
+                    }
+                >
+                    {answers.map((answer, index) => (
+                        <button
+                            key={index}
+                            className={getButtonClass(answer)}
+                            onClick={checkAnswer}
+                            disabled={showResult}
+                        >
+                            {answer}
+                        </button>
+                    ))}
+                </div>
+
+                <div className={styles.scorePanel}>
+                    <span>Score: {score}</span>
+                </div>
+                <div>
+                    <button
+                        className={styles.resetQuizButton}
+                        onClick={restartQuizSameSettings}
+                    >
+                        Restart Quiz
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 };
